63fe8bfac85e04d44ae5b6597f2e02cd131cbd47
Mohamed BASSAOUD <mohamed.bassaoud@yahoo.fr>
2019-12-21 13:02:30 +0100

    first commit

---
+++ src/main/java/maven/App.java
@@ -0,0 +1,18 @@
+package maven;
+
+import java.util.ResourceBundle;
+import com.github.lalyos.jfiglet.FigletFont;
+
+public class App {
+
+   public static void main(String[] args) {
+     
+     String titre = ResourceBundle.getBundle("application").getString("titre");
+     String asciiArt = FigletFont.convertOneLine(titre);
+     System.out.println(asciiArt);
+     
+     
+     String environnement = ResourceBundle.getBundle("application").getString("environnement");
+     System.out.println("Environnement : " + environnement);
+  }
+}
